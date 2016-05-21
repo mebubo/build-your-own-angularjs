e7c3226b94fff1c276c68c5a1f3bd3b2d0296af0
Sergei Dolgov <dolgovs@gmail.com>
2016-05-21 22:59:02 +0200

    Check for dirty values

--- src/scope.js
+++ src/scope.js
@@ -17,8 +17,12 @@ Scope.prototype.$watch = function(watchFn, listenerFn) {
 Scope.prototype.$digest = function() {
   var self = this;
   _.forEach(this.$$watchers, function(watcher) {
-    watcher.watchFn(self);
-    watcher.listenerFn();
+    var newValue = watcher.watchFn(self);
+    var oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.listenerFn(newValue, oldValue, self);
+      watcher.last = newValue;
+    }
   });
 };
 
